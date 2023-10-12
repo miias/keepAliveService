3cfb094ffb9a8c70518c0b7fe13fb4bf314bae69
miias.hubi <miias.hubi@gmail.com>
2023-10-12 22:47:46 +0300

    c

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+const WEBSITE_URL = 'https://dynamic-helpful-drill.glitch.me';
+const PING_INTERVAL_MS = 60000; // 1 minute interval (adjust as needed)
+
+function pingWebsite() {
+    axios.get(WEBSITE_URL)
+        .then(response => {
+            console.log(`Ping successful at ${new Date()}`);
+        })
+        .catch(error => {
+            console.error(`Failed to ping ${WEBSITE_URL}. Error: ${error.message}`);
+        });
+}
+
+// Initial ping when the server starts
+pingWebsite();
+
+// Schedule automatic pings at the specified interval
+setInterval(pingWebsite, PING_INTERVAL_MS);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
